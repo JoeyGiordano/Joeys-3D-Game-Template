d77aa11c5529cc940fd5c202e94b636e042aba98
JoeyGiordano <jgzgiordano@gmail.com>
2023-12-28 02:06:04 -0500

    Camera Galore
    Todo
    Put 1st person cam on cinmach system
    Prefabize
    Should now be really easy to Fix visual bug with the grapple tip.
    
    Make sprint, crouch (separate script)
    Add other MovementStates
    Get rid of speed UI in player prefab
    Fork
    Delete all old stuff

--- Assets/Player/Scripts/Camera/PlayerCam3rd.cs
+++ Assets/Player/Scripts/Camera/PlayerCam3rd.cs
@@ -4,17 +4,114 @@ using UnityEngine;
 
 public class PlayerCam3rd : MonoBehaviour
 {
-    public string temp = "NOT YET IMPLEMENTED";
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerModel;
+
+    [Header("Other References")]
+    public Transform brain;
+    public Transform combatLookAt;
+
+    [Header("Cameras")]
+    public GameObject basicCam;
+    public GameObject lockedBehindCam;
+    public GameObject combatCam;
+    public GameObject topDownCam;
+
+    [Header("Settings")]
+    public float rotationSpeed;
+    public CameraStyle currentStyle;
+
+    public enum CameraStyle
     {
+        Basic,
+        LockedBehind,
+        Combat,
+        Topdown
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        SwitchCameraStyle(currentStyle);
+    }
+
+    private void Update()
+    {
+        //Input
+        ProcessStyleSwitchInput();
+
+        //Rotations
+        RotatePlayerOrientation();
+        RotatePlayerModel();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RotatePlayerOrientation()
     {
+        Vector3 viewDirection = player.position - brain.position;
+        viewDirection.y = 0;
+        orientation.forward = viewDirection.normalized;
+    }
+
+    private void RotatePlayerModel()
+    {
+        if (currentStyle == CameraStyle.Basic || currentStyle == CameraStyle.Topdown)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            //rotate the player towards its movement input direction (s is walking towards the camera, not walking backwards) 
+            if (inputDir != Vector3.zero)
+                SlerpPlayerModelRotationTo(inputDir.normalized);
+        }
+
+        else if (currentStyle == CameraStyle.LockedBehind)
+        {
+            playerModel.forward = orientation.forward;
+        }
+
         
+        else if (currentStyle == CameraStyle.Combat)
+        {
+            Vector3 combatViewDirection = player.position - brain.position;
+            combatViewDirection.y = 0;
+            orientation.forward = combatViewDirection.normalized;
+            
+            playerModel.forward = combatViewDirection.normalized;
+        }
+    }
+
+    private void ProcessStyleSwitchInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchCameraStyle(CameraStyle.Basic);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchCameraStyle(CameraStyle.LockedBehind);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCameraStyle(CameraStyle.Combat);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) SwitchCameraStyle(CameraStyle.Topdown);
+
+    }
+
+    public void SwitchCameraStyle(CameraStyle newStyle)
+    {
+        basicCam.SetActive(false);
+        lockedBehindCam.SetActive(false);
+        combatCam.SetActive(false);
+        topDownCam.SetActive(false);
+
+        if (newStyle == CameraStyle.Basic) basicCam.SetActive(true);
+        if (newStyle == CameraStyle.LockedBehind) lockedBehindCam.SetActive(true);
+        if (newStyle == CameraStyle.Combat) combatCam.SetActive(true);
+        if (newStyle == CameraStyle.Topdown) topDownCam.SetActive(true);
+
+        currentStyle = newStyle;
+    }
+
+    private void SlerpPlayerModelRotationTo(Vector3 targetRotation)
+    {
+        playerModel.forward = Vector3.Slerp(playerModel.forward, targetRotation, Time.deltaTime * rotationSpeed);
     }
 }
